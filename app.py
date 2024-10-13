c30948cc474b95f33faa6b937c9b9b517e990bd4
ricardojaj <rickanjos85@gmail.com>
2024-10-12 22:30:20 -0300

    Novas rotas para o carrinho - visualizar, deletar e limpar

--- app.py
+++ app.py
@@ -162,10 +162,50 @@ def add_to_cart(product_id):
 
 
     if user and product:
+        cart_item = CartItem(user_id=user.id, product_id= product.id)
+        db.session.add(cart_item)
+        db.session.commit()
         return jsonify({'message': 'Item added to the cart successfully'}), 200
     return jsonify({'message': 'Failed to add item to the cart'}), 400
 
 
+@app.route('/api/cart/remove/<int:product_id>', methods=["DELETE"])
+@login_required 
+def delete_item(product_id):
+    cart_item = CartItem.query.filter_by(user_id=current_user.id, product_id=product_id).first()
+    if cart_item:
+        db.session.delete(cart_item)
+        db.session.commit()
+        return jsonify ({"message": "Item removed from the cart successfully."})
+    return jsonify({"message": "Failed to remove item from the cart"}), 404
+
+
+@app.route('/api/cart', methods=['GET'])
+@login_required
+def view_cart():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
+    cart_content = []
+    for cart_item in cart_items:
+        product = Product.query.get(cart_item.product_id)
+        cart_content.append({
+                                "id": cart_item.id,
+                                "user_id": cart_item.user_id,
+                                "product_id": cart_item.product_id,
+                                "product_name": product.name,
+                                "product_price": product.price
+                            })
+    return jsonify(cart_content)
+
+@app.route('/api/cart/checkout', methods=['POST'])
+@login_required
+def checkout():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
+    for cart_item in cart_items:
+        db.session.delete(cart_item)
+    db.session.commit()
+    return jsonify({'message': 'Checkout successful. Cart has been cleared.'})
 
 # Disponibilizando api para ser utilizada no modo debug
 if __name__ == "__main__":
